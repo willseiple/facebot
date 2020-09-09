60eaf46d7eec43a9947b5d78598ac967d474f7c7
Will Seiple <60242249+willseiple@users.noreply.github.com>
2020-09-08 21:56:35 -0400

    added facebot.sh

---
+++ facebot.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+fileId=16S6IvJlvc8JOq-qh3-BDYIC67VjAQEJH
+fileName=data.zip
+
+if [ ! -d 'reader/data' ]
+then
+    printf 'Fetching files from Google Drive\n'
+    curl -sc /tmp/cookie "https://drive.google.com/uc?export=download&id=${fileId}" > /dev/null
+    code="$(awk '/_warning_/ {print $NF}' /tmp/cookie)"
+    printf 'Downloading...\n'
+    curl -sLb /tmp/cookie "https://drive.google.com/uc?export=download&confirm=${code}&id=${fileId}" -o ${fileName}
+    printf 'Unzipping files...\n'
+    unzip data.zip -d reader
+    printf 'Installation complete\n'
+fi
+
+python3 cli.py
